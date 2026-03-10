8b94bed5ae41caf140fceda1bbdde37d064b2194
Uminda <hello@uminda.dev>
2026-03-10 14:37:36 +0530

    Configure for Cloudflare Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const withCloudflare = require('@cloudflare/next-on-pages')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -5,4 +7,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = withCloudflare(nextConfig)
